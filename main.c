afdf9eb57a5d6d9d57863f66e54f183fcdf93033
Gabriel <gabriel.touzalin19@gmail.com>
2026-07-23 15:51:40 +0200

    done

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 enum Command
-{ ADD, SUB, MUL, DOT, CROSS, LEN, NORM, INVALID };
+{ ADD, SUB, MUL, DOT, CROSS, LEN, NORM, INVALID, DT, STEP };
 
 enum Command
 getCommand(const char *op)
@@ -15,9 +15,13 @@ getCommand(const char *op)
     if (strcmp(op, "NORM") == 0) return NORM;
     if (strcmp(op, "LEN") == 0) return LEN;
     if (strcmp(op, "DOT") == 0) return DOT;
+    if (strcmp(op, "DT") == 0) return DT;
+    if (strcmp(op, "STEP") == 0) return STEP;
     return INVALID;
 }
 
+//  Should use <math.h> but the tests don't compile with -lm
+//  hence this function.
 double
 my_sqrt(double x)
 {
@@ -40,12 +44,15 @@ main(void)
 {
     char line[1024];
 
+    double dt, mass = 0;
+
     while (fgets(line, sizeof line, stdin))
     {
         if (line[0] == '\n' || line[0] == '\0') continue;
 
         char op[6];
         double ax, ay, bx, by, s;
+        double px, py, vx, vy, fx, fy;
 
         if (sscanf(line, "%5s", op) != 1) continue;
 
@@ -89,6 +96,27 @@ main(void)
                         printf("0.0000 0.0000\n");
                     else
                         printf("%.4f %.4f\n", ax / len, ay / len);
+                }
+                break;
+
+            case DT:
+                sscanf(line, "%*s %lf %*s %lf", &dt, &mass);
+                break;
+
+            case STEP:
+                if (sscanf(line, "%*s %lf %lf %lf %lf %lf %lf",
+                            &px, &py, &vx, &vy, &fx, &fy) == 6)
+                {
+                    ax = fx / mass;
+                    ay = fy / mass;
+
+                    vx += ax * dt;
+                    vy += ay * dt;
+
+                    px += vx * dt;
+                    py += vy * dt;
+
+                    printf("%.4f %.4f %.4f %.4f\n", px, py, vx, vy);
                 }
                 break;
 
