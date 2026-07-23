16db3c660a2e09791ce550043dce5b0d3583740e
Gabriel <gabriel.touzalin19@gmail.com>
2026-07-23 15:13:29 +0200

    done

--- main.c
+++ main.c
@@ -2,13 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* TODO (what-is-physics): implement per the lesson description. */
-
-int main(void) {
+int
+main(void)
+{
     char line[1024];
-    while (fgets(line, sizeof line, stdin)) {
-        if (line[0] == '\n' || line[0] == 0) continue;
-        printf("TODO\n");
+
+    while (fgets(line, sizeof line, stdin))
+    {
+        if (line[0] == '\n' || line[0] == '\0') continue;
+
+        char op[4];
+        double ax, ay, bx, by, s;
+
+        if (sscanf(line, "%3s", op) != 1) continue;
+
+        if (strcmp(op, "ADD") == 0)
+        {
+            if (sscanf(line, "%*s %lf %lf %lf %lf", &ax, &ay, &bx, &by) == 4)
+                printf("%.4f %.4f\n", ax + bx, ay + by);
+        }
+        else if (strcmp(op, "SUB") == 0)
+        {
+            if (sscanf(line, "%*s %lf %lf %lf %lf", &ax, &ay, &bx, &by) == 4)
+                printf("%.4f %.4f\n", ax - bx, ay - by);
+        }
+        else if (strcmp(op, "MUL") == 0)
+        {
+            if (sscanf(line, "%*s %lf %lf %lf", &ax, &ay, &s) == 3)
+                printf("%.4f %.4f\n", ax * s, ay * s);
+        }
     }
-    return 0;
+
+    return (0);
 }
