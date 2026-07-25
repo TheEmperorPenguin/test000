9a8ac57ee9513a72f47d744f21ce939643e1c642
Gabriel <gabriel.touzalin19@gmail.com>
2026-07-25 22:27:38 +0200

    done

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 enum Command
-{ ADD, SUB, MUL, DOT, CROSS, LEN, NORM, INVALID, DT, STEP };
+{ ADD, SUB, MUL, DOT, CROSS, LEN, NORM, INVALID, DT, STEP, INIT, FORCE };
 
 enum Command
 getCommand(const char *op)
@@ -17,8 +16,23 @@ getCommand(const char *op)
     if (strcmp(op, "DOT") == 0) return DOT;
     if (strcmp(op, "DT") == 0) return DT;
     if (strcmp(op, "STEP") == 0) return STEP;
+    if (strcmp(op, "INIT") == 0) return INIT;
+    if (strcmp(op, "FORCE") == 0) return FORCE;
     return INVALID;
 }
+
+typedef
+struct vector_2d
+{   double x;
+    double y;
+} vector_2d;
+
+typedef
+struct rigid_body_2d
+{   vector_2d position;
+    vector_2d velocity;
+} rigid_body_2d;
+
 
 //  Should use <math.h> but the tests don't compile with -lm
 //  hence this function.
@@ -38,6 +52,115 @@ my_sqrt(double x)
 
     return guess;
 }
+vector_2d
+add_vector_2d(vector_2d a, vector_2d b)
+{
+    vector_2d result;
+
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+
+    return result;
+}
+
+vector_2d
+sub_vector_2d(vector_2d a, vector_2d b)
+{
+    vector_2d result;
+
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+
+    return result;
+}
+
+vector_2d
+mul_vector_2d(vector_2d v, double scalar)
+{
+    vector_2d result;
+
+    result.x = v.x * scalar;
+    result.y = v.y * scalar;
+
+    return result;
+}
+
+double
+dot_vector_2d(vector_2d a, vector_2d b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+double
+cross_vector_2d(vector_2d a, vector_2d b)
+{
+    return a.x * b.y - a.y * b.x;
+}
+
+double
+length_vector_2d(vector_2d v)
+{
+    return my_sqrt(v.x * v.x + v.y * v.y);
+}
+
+vector_2d
+normalize_vector_2d(vector_2d v)
+{
+    vector_2d result = {0, 0};
+
+    double len = length_vector_2d(v);
+
+    if (len != 0)
+    {
+        result.x = v.x / len;
+        result.y = v.y / len;
+    }
+
+    return result;
+}
+
+void print_vector_2d(vector_2d v)
+{
+    printf("%.4f %.4f\n", v.x, v.y);
+}
+
+rigid_body_2d
+step_body_explicit(rigid_body_2d body, vector_2d force, double mass, double dt)
+{
+    vector_2d acceleration = mul_vector_2d(force, 1.0 / mass);
+
+    // Position uses OLD velocity
+    body.position = add_vector_2d(
+        body.position,
+        mul_vector_2d(body.velocity, dt)
+    );
+
+    // Then update velocity
+    body.velocity = add_vector_2d(
+        body.velocity,
+        mul_vector_2d(acceleration, dt)
+    );
+
+    return body;
+}
+
+rigid_body_2d
+step_body_symplectic(rigid_body_2d body, vector_2d force, double mass, double dt)
+{
+    vector_2d acceleration = mul_vector_2d(force, 1.0 / mass);
+
+    body.velocity = add_vector_2d(
+        body.velocity,
+        mul_vector_2d(acceleration, dt)
+    );
+
+    body.position = add_vector_2d(
+        body.position,
+        mul_vector_2d(body.velocity, dt)
+    );
+
+    return body;
+}
 
 int
 main(void)
@@ -51,72 +174,130 @@ main(void)
         if (line[0] == '\n' || line[0] == '\0') continue;
 
         char op[6];
-        double ax, ay, bx, by, s;
-        double px, py, vx, vy, fx, fy;
+        int steps;
+        vector_2d a, b, result;
+        double s;
+        rigid_body_2d body;
 
         if (sscanf(line, "%5s", op) != 1) continue;
 
         switch (getCommand(op))
         {
             case ADD:
-                if (sscanf(line, "%*s %lf %lf %lf %lf", &ax, &ay, &bx, &by) == 4)
-                    printf("%.4f %.4f\n", ax + bx, ay + by);
+                if (sscanf(line, "%*s %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y) == 4)
+                {
+                    result = add_vector_2d(a, b);
+                    print_vector_2d(result);
+                }
                 break;
 
             case SUB:
-                if (sscanf(line, "%*s %lf %lf %lf %lf", &ax, &ay, &bx, &by) == 4)
-                    printf("%.4f %.4f\n", ax - bx, ay - by);
+                if (sscanf(line, "%*s %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y) == 4)
+                {
+                    result = sub_vector_2d(a, b);
+                    print_vector_2d(result);
+                }
                 break;
 
             case MUL:
-                if (sscanf(line, "%*s %lf %lf %lf", &ax, &ay, &s) == 3)
-                    printf("%.4f %.4f\n", ax * s, ay * s);
+                if (sscanf(line, "%*s %lf %lf %lf", &a.x, &a.y, &s) == 3)
+                {
+                    result = mul_vector_2d(a, s);
+                    print_vector_2d(result);
+                }
                 break;
 
             case DOT:
-                if (sscanf(line, "%*s %lf %lf %lf %lf", &ax, &ay, &bx, &by) == 4)
-                    printf("%.4f\n", ax * bx + ay * by);
+                if (sscanf(line, "%*s %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y) == 4)
+                {
+                    double dot = dot_vector_2d(a, b);
+                    printf("%.4f", dot);
+                }
                 break;
 
             case CROSS:
-                if (sscanf(line, "%*s %lf %lf %lf %lf", &ax, &ay, &bx, &by) == 4)
-                    printf("%.4f\n", ax * by - ay * bx);
+                if (sscanf(line, "%*s %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y) == 4)
+                {
+                    double cross = cross_vector_2d(a, b);
+                    printf("%.4f", cross);
+                }
                 break;
 
             case LEN:
-                if (sscanf(line, "%*s %lf %lf", &ax, &ay) == 2)
-                    printf("%.4f\n", my_sqrt(ax * ax + ay * ay));
+                if (sscanf(line, "%*s %lf %lf", &a.x, &a.y) == 2)
+                {
+                    double len = length_vector_2d(a);
+                    printf("%.4f", len);
+                }
                 break;
 
             case NORM:
-                if (sscanf(line, "%*s %lf %lf", &ax, &ay) == 2)
+                if (sscanf(line, "%*s %lf %lf", &a.x, &a.y) == 2)
                 {
-                    double len = my_sqrt(ax * ax + ay * ay);
-                    if (len == 0)
-                        printf("0.0000 0.0000\n");
-                    else
-                        printf("%.4f %.4f\n", ax / len, ay / len);
+                    result = normalize_vector_2d(a);
+                    print_vector_2d(result);
                 }
                 break;
 
             case DT:
-                sscanf(line, "%*s %lf %*s %lf", &dt, &mass);
+                if (sscanf(line, "%*s %lf %*s %d %*s %lf", &dt, &steps, &mass) == 3)
+                {}
+                else if (sscanf(line, "%*s %lf %*s %lf", &dt, &mass) == 2)
+                {}
                 break;
 
             case STEP:
-                if (sscanf(line, "%*s %lf %lf %lf %lf %lf %lf",
-                            &px, &py, &vx, &vy, &fx, &fy) == 6)
+                if (sscanf(line,
+                    "%*s %lf %lf %lf %lf %lf %lf",
+                    &body.position.x, &body.position.y,
+                    &body.velocity.x, &body.velocity.y,
+                    &a.x, &a.y) == 6)
                 {
-                    ax = fx / mass;
-                    ay = fy / mass;
+                    body = step_body_symplectic(body, a, mass, dt);
 
-                    vx += ax * dt;
-                    vy += ay * dt;
+                    printf("%.4f %.4f %.4f %.4f\n",
+                        body.position.x,
+                        body.position.y,
+                        body.velocity.x,
+                        body.velocity.y);
+                }
+                break;
 
-                    px += vx * dt;
-                    py += vy * dt;
+            case INIT:
+                if (sscanf(line, "%*s %lf %lf %lf %lf",
+                            &body.position.x,
+                            &body.position.y,
+                            &body.velocity.x,
+                            &body.velocity.y) == 4
+                )
+                {}
+                break;
 
-                    printf("%.4f %.4f %.4f %.4f\n", px, py, vx, vy);
+            case FORCE:
+                if (sscanf(line, "%*s %lf %lf", &a.x, &a.y) == 2)
+                {
+                    int i;
+
+                    rigid_body_2d explicit_body = body;
+                    rigid_body_2d symplectic_body = body;
+
+                    for (int i = 0; i < steps; i++ )
+                    {
+                        explicit_body = step_body_explicit(explicit_body, a, mass, dt);
+                        symplectic_body = step_body_symplectic(symplectic_body, a, mass, dt);
+                    }
+
+                    printf("EXPLICIT %.4f %.4f %.4f %.4f\n",
+                        explicit_body.position.x,
+                        explicit_body.position.y,
+                        explicit_body.velocity.x,
+                        explicit_body.velocity.y);
+
+                    printf("SYMPLECTIC %.4f %.4f %.4f %.4f\n",
+                        symplectic_body.position.x,
+                        symplectic_body.position.y,
+                        symplectic_body.velocity.x,
+                        symplectic_body.velocity.y);
                 }
                 break;
 
